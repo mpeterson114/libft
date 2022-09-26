d793c3af90595bf7f6a8960b3a87e723f68940d8
Megan Peterson <mpeterso@c3r8s4.42madrid.com>
2022-09-26 12:41:27 +0200

    memchr thru atoi started with headers

---
+++ ft_strncmp.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeterso <mpeterso@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 11:56:46 by mpeterso          #+#    #+#             */
+/*   Updated: 2022/09/26 12:25:51 by mpeterso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1|| s2))
+	{
+		if (s1[i] > s2[i])
+		{
+			return (1);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/*int main()
+{
+	const char s1[] = "cola";
+	const char s2[] = "cola";
+	printf("%d", ft_strncmp(s1, s2, 4));
+	return 0;
+}*/
+
+		
