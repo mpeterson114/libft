c11ffbc407d07c2497c0eacc5990ce31847a698d
mpeterson114 <mlpeterson114@gmail.com>
2022-09-26 12:41:27 +0200

    memchr thru atoi started with headers

---
+++ ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeterso <mpeterso@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 12:30:29 by mpeterso          #+#    #+#             */
+/*   Updated: 2022/09/26 12:39:49 by mpeterso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	is_neg;
+	int	result;
+
+	if(str == NULL)
+	{
+		return (0);
+	}
+	
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+	{
+		i++;
+	}
+	is_neg = (str[i] == '-') ? -1 : 1;
+	if (is_neg == -1 || str[i] == '+')
+	{
+		i++;
+	}
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * is_neg);
+}
+
+/*int main()
+{
+	const char str[] = "  \n   +25b8";
+	printf("%d", ft_atoi(str));
+	return (0);
+}*/
