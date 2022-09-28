8dad3780b39cc2b65e0fd0d1bc308832d609e477
Megan Peterson <mpeterso@c3r7s5.42madrid.com>
2022-09-28 20:20:44 +0200

    isascii isprint memchr memset strncmp passed test

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:27:53 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/09/26 12:28:37 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/09/28 20:16:51 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,30 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	size_t	i;
+
+	unsigned int	diff;
+
+	i = 0;
+	diff = 0;
+	while (i < n && diff == 0 && s1[i] != '\0' && s2[i] != '\0')
+	{
+		diff = ((unsigned char *)s1)[i] - ((unsigned char*)s2)[i];
+		i++;
+	}
+	while (i < n && diff == 0 && (s1[i] == 0 || s2[i] == 0))
+	{
+		diff = ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i];
+	}
+	return(diff);
+}
+
+int main()
+{
+	const char s1[] = "hola";
+	const char s2[] = "cola";
+	size_t n = 3;
+	printf("%d", ft_memcmp(s1, s2, n));
+	return 0;
+}
 
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:56:46 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/09/26 12:25:51 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/09/28 20:13:48 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,26 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned int	diff;
 
 	i = 0;
-	while (i < n && (s1|| s2))
+	diff = 0;
+	while (i < n && diff == 0 && (s1[i] != '\0') && (s2[i] != '\0'))
 	{
-		if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
+		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
 		i++;
 	}
-	return (0);
+	while (i < n && diff == 0 && (s1[i] == 0 || s2[i] == 0))
+	{
+		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+	}
+	return(diff);
 }
 
 /*int main()
 {
-	const char s1[] = "cola";
+	const char s1[] = "hola";
 	const char s2[] = "cola";
 	printf("%d", ft_strncmp(s1, s2, 4));
 	return 0;
-}*/
-
-		
+}	*/
