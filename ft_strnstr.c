d0bae101aabfd5062c463ccea16bdb90325f118b
mpeterson114 <mlpeterson114@gmail.com>
2022-10-03 12:02:19 +0200

    memcmp stnstr done, check test

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,4 +14,32 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t h;
+	size_t n;
 
+	h = 0;
+	if (needle == NULL)
+		return(char *)haystack;
+	while (haystack[h] != '\0')
+	{ 
+		n = 0;
+		while (haystack[h + n] == needle[n] && (h + n) < len)
+		{
+			if(haystack[h + n] == '\0' && needle[n] == '\0')
+				return(char *)&haystack[h];
+			n++;
+		}
+		if (needle[n] == '\0')
+			return((char*)haystack + h);
+		h++;
+	}
+	return(NULL);
+}
+
+int main()
+{
+	const char haystack[] = "what's up";
+	const char needle[] = "up";
+	size_t len = 10;
+	printf("%s", ft_strnstr(haystack, needle, len));
+	return 0;}
