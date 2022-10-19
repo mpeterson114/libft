88c62d69f876fa17f9c80cfae1b208f432093d01
mpeterson114 <mlpeterson114@gmail.com>
2022-10-19 13:38:52 +0200

    strncmp compiles, needs test

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:56:46 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/10/18 13:07:36 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/19 13:38:21 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,39 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	diff;
 	size_t			i;
 
 	i = 0;
-	diff = 0;
-	while (i < n && diff == 0 && (s1[i] != '\0') && (s2[i] != '\0'))
+	if (n == 0)
 	{
-		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
-		i++;
+		return (0);
 	}
-	while (i < n && diff == 0 && (s1[i] == 0 || s2[i] == 0))
+	while (i < n && s1[i] != '0')
 	{
-		diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+		if (s1[i] == s2[i])
+			i++;
+		else
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+/*{
+		
+		{
+			diff = 
+			i++;
+		}
+		else if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			diff = (unsigned char)s1[i] - (unsigned char)s2[i];
+		}
 	}
 	return (diff);
-}
 
-/*int main()
+
+int main()
 {
 	const char s1[] = "hola";
 	const char s2[] = "cola";
