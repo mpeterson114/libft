9724c106eb671e4149fc613ab3aeac8cc07ed39d
Megan Peterson <mpeterso@c3r10s6.42madrid.com>
2022-10-19 11:35:10 +0200

    strnstr good

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:28:57 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/10/18 13:08:42 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/19 11:33:28 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	n;
 
 	h = 0;
-	if (needle == NULL)
+	if (needle[0] == '\0')
 		return ((char *)haystack);
 	while (haystack[h] != '\0')
 	{
@@ -39,7 +39,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 /*int main()
 {
 	const char haystack[] = "what's up";
-	const char needle[] = "up";
+	const char needle[] = "\0";
 	size_t len = 10;
 	printf("%s", ft_strnstr(haystack, needle, len));
 	return 0;}*/
