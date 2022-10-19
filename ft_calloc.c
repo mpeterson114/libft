23f7ee70bd12877697661ad44e8f72c9d859e59c
mpeterson114 <mlpeterson114@gmail.com>
2022-10-19 10:58:06 +0200

    calloc fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 11:01:58 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/10/18 12:09:14 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/19 10:57:23 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	total;
 
-	ptr = (void *)malloc(count * size);
-	if (ptr == 0)
-		return (NULL);
-	ft_bzero(ptr, count);
+	total = count * size;
+	ptr = (void *)malloc(total);
+	if (!ptr)
+		return (0);
+	ft_bzero(ptr, total);
 	return (ptr);
 }
