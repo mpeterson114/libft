2be46cb5108c08354ae63a1143e1597753a2c1a7
Megan Peterson <mpeterso@c3r9s6.42madrid.com>
2022-10-20 10:45:49 +0200

    strncmp passes francinette

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:56:46 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/10/19 13:38:21 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/20 10:45:14 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,21 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t			i;
 
 	i = 0;
-	if (n == 0)
+	if (n == 0 || (!s1 && !s2))
 	{
 		return (0);
 	}
-	while (i < n && s1[i] != '0')
+	while (i < n -1 && (s1[i] == s2[i]) && s1[i] != '\0')
 	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
+		i++;
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-/*{
-		
-		{
-			diff = 
-			i++;
-		}
-		else if (s1[i] == '\0' && s2[i] == '\0')
-		{
-			diff = (unsigned char)s1[i] - (unsigned char)s2[i];
-		}
-	}
-	return (diff);
 
-
-int main()
+/*int main()
 {
-	const char s1[] = "hola";
-	const char s2[] = "cola";
-	printf("%d", ft_strncmp(s1, s2, 4));
-	return 0;
-}	*/
+	const char s1[] = "abcdef";
+	const char s2[] = "abcdefghijklm";
+	printf("%d\n", ft_strncmp(s1, s2, 6));
+	printf("%d\n", strncmp(s1, s2, 6));
+	return 0;}*/
