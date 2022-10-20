26ae2b0aaccc4bf19c38b4b44f2514098c38170c
mpeterson114 <mlpeterson114@gmail.com>
2022-10-20 13:28:48 +0200

    strchr and strrchr not seg faulting w null param

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 11:49:00 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/10/18 13:04:08 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/20 13:05:10 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
-	if (s == 0)
+	if (s == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
 	i = 0;
 	while (s[i] != '\0')
@@ -31,12 +31,12 @@ char	*ft_strchr(const char *s, int c)
 	{
 		return ((char *)(s + i));
 	}
-	return (NULL);
+	return (0);
 }
 
 /*int main()
 {
 	const char s[] = "cheese";
-	int c = 'h';
+	int c = '\0';
 	printf("%s", ft_strchr(s, c));
 	return 0;}*/	
