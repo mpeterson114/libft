9eae7ee2db563709cfcab0a0eed5b748edafe30e
Megan Peterson <mpeterso@c3r9s6.42madrid.com>
2022-10-20 12:43:14 +0200

    atoi modifications, passes test I think

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mpeterso <mpeterso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:30:29 by mpeterso          #+#    #+#             */
-/*   Updated: 2022/09/26 13:13:02 by mpeterso         ###   ########.fr       */
+/*   Updated: 2022/10/20 12:42:17 by mpeterso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,28 @@
 int	ft_atoi(const char *str)
 {
 	int	i;
-	int	is_neg;
+	int	sign;
 	int	result;
 
 	if (str == NULL)
 		return (0);
 	i = 0;
-	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-')
-		is_neg = -1;
-	else
-		is_neg = 1;
-	if (is_neg == -1 || str[i] == '+')
+	sign = 1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
+	}
 	result = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = (result * 10) + (str[i] - '0');
 		i++;
 	}
-	return (result * is_neg);
+	return (result * sign);
 }
 
 /*int main()
